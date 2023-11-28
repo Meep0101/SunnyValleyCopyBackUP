7fe6c961828552b0bb16699d250622d4d1e416ee
jenmD12 <jenillemae09@gmail.com>
2023-11-28 21:07:14 +0800

    MapSelection Code
    added scenemanager script for back and playmap

--- Assets/Scripts/MapSelectionScripts/MapChoice.cs
+++ Assets/Scripts/MapSelectionScripts/MapChoice.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainChoice : MonoBehaviour
+public class MapChoice : MonoBehaviour
 {
     public void PlayMap()
     {
