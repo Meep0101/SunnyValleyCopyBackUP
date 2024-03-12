197a30a16aab72c4d4da3209a8d6ad70012ef09c
Pepperminteppy <stephaniejoyce.buenaobra.v@bulsu.edu.ph>
2024-03-13 04:13:24 +0800

    CarbonMeter Update

--- Assets/Scripts/CarbonMeter.cs
+++ Assets/Scripts/CarbonMeter.cs
@@ -1,33 +1,118 @@
-using System.Collections;
-using System.Collections.Generic;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// public class CarbonMeter : MonoBehaviour
+// {
+//     public Text carbonMeterText; // Reference to the UI Text element
+
+//     private int carbonMeterValue = 80;
+
+//      void Start()
+//     {
+//         UpdateCarbonMeter();
+//     }
+
+//     // Function to update the carbon meter UI
+
+//     public void UpdateCarbonMeter()
+//     {
+//         if (carbonMeterText != null)
+//         {
+//             carbonMeterText.text = "Carbon Emission:" + carbonMeterValue +"%";
+
+//             if (carbonMeterValue >= 100)
+//             {
+//                 StopGame();
+//             }
+        
+//         }
+//     }
+
+//     // Function to increase the carbon meter value
+//     public void IncreaseCarbonMeter()
+//     {
+//         carbonMeterValue++;
+//         UpdateCarbonMeter(); // Update UI when CarbonMeter changes
+//     }
+
+//     public void DecreaseCarbonMeter()
+//     {
+//         carbonMeterValue--;
+//         UpdateCarbonMeter();
+//     }
+
+//     // Function to reset the carbon meter value
+//     public void ResetCarbonMeter()
+//     {
+//         carbonMeterValue = 80;
+//         UpdateCarbonMeter(); // Update UI when CarbonMeter changes
+//     }
+
+//     private void StopGame()
+//     {
+//         Debug.Log("STOP NA AYOKO NA");
+//     }
+//     public int GetCarbonMeterValue()
+//     {
+//         return carbonMeterValue;
+
+//     }
+
+   
+// }
+
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CarbonMeter : MonoBehaviour
 {
-    public Text carbonMeterText; // Reference to the UI Text element
+    public Slider carbonMeterSlider; // Reference to the UI Slider element
+    public Text carbonMeterText;     // Reference to the UI Text element
 
     private int carbonMeterValue = 80;
 
-     void Start()
+    void Start()
     {
-        UpdateCarbonMeter();
+        InitializeCarbonMeter();
     }
 
-    // Function to update the carbon meter UI
+    // Function to initialize the carbon meter UI
+    void InitializeCarbonMeter()
+    {
+        if (carbonMeterSlider != null)
+        {
+            carbonMeterSlider.maxValue = 100;
+            carbonMeterSlider.value = carbonMeterValue;
+        }
 
-    public void UpdateCarbonMeter()
+        UpdateCarbonMeterText();
+    }
+
+    // Function to update the carbon meter UI text
+    void UpdateCarbonMeterText()
     {
         if (carbonMeterText != null)
         {
-            carbonMeterText.text = "Carbon Emission:" + carbonMeterValue +"%";
+            carbonMeterText.text = "Carbon Emission: " + carbonMeterValue + "%";
 
             if (carbonMeterValue >= 100)
             {
                 StopGame();
             }
-        
         }
+    }
+
+    // Function to update the carbon meter UI slider
+    public void UpdateCarbonMeter()
+    {
+        if (carbonMeterSlider != null)
+        {
+            carbonMeterSlider.value = carbonMeterValue;
+        }
+
+        UpdateCarbonMeterText();
     }
 
     // Function to increase the carbon meter value
@@ -37,6 +122,7 @@ public class CarbonMeter : MonoBehaviour
         UpdateCarbonMeter(); // Update UI when CarbonMeter changes
     }
 
+    // Function to decrease the carbon meter value
     public void DecreaseCarbonMeter()
     {
         carbonMeterValue--;
@@ -54,11 +140,9 @@ public class CarbonMeter : MonoBehaviour
     {
         Debug.Log("STOP NA AYOKO NA");
     }
+
     public int GetCarbonMeterValue()
     {
         return carbonMeterValue;
-
     }
-
-   
 }
